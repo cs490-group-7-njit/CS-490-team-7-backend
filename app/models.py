08241a4146f8e2b7f516fb12f175e56c9e7da5bc
Jossie Zamora <jz574@njit.edu>
2025-10-27 17:45:20 +0000

    UC 1.6: Fix user_id column to be nullable in Staff model

--- app/models.py
+++ app/models.py
@@ -124,7 +124,7 @@ class Staff(db.Model):
 
     staff_id = db.Column(db.Integer, primary_key=True)
     salon_id = db.Column(db.Integer, db.ForeignKey("salons.salon_id"), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=True)
     title = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=utc_now)
     updated_at = db.Column(
